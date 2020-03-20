2bd2b3795306cc65c168b7058612b77691490959
obliges <rlghks1710@kaist.ac.kr>
2020-03-20 04:42:13 -0700

    cipher implemented

--- client.c
+++ client.c
@@ -1,4 +1,59 @@
-int main() {
+#include<string.h>
+#include<ctype.h>
+#include<stdio.h>
 
+char* encrypt(char* plainmessage, int n);
+char* decrypt(char* ciphermessage, int n);
+
+
+
+int main(int argc, char **argv) {
+    char koz[100] = "abcdefGHIJKLMNOPQrstuvwxyz";
+
+    printf(encrypt(koz, 1));
+    printf("\n");
+    printf(decrypt(koz, -1));
+    printf("\n");
     return 0;
+}
+
+
+char* encrypt(char* plainmessage, int n) {
+    int move = n%26;
+    char* temp = plainmessage;
+    for (int i = 0; temp[i] != '\0'; i++) {
+	if (isalpha(temp[i])) {
+	    temp[i] = tolower(temp[i]) - (26-move);
+	    if (temp[i] > 'z') {
+		temp[i] = temp[i] - 'z' + 'a' - 1;
+	    }
+	    else if (temp[i] < 'a') {
+		temp[i] = temp[i] + 'z' - 'a' + 1;
+	    }	    	
+	}
+	else {
+	    continue;
+	}
+    }
+    return temp;
+}
+
+char* decrypt(char* ciphermessage, int n) {
+    int move = n%26;
+    char* temp = ciphermessage;
+    for (int i = 0; temp[i] != '\0'; i++) {
+	if (isalpha(temp[i])) {
+	    temp[i] = tolower(temp[i]) - move;
+	    if (temp[i] > 'z') {
+		temp[i] = temp[i] - 'z' + 'a' - 1;
+	    }
+	    else if (temp[i] < 'a') {
+		temp[i] = temp[i] + 'z' - 'a' + 1;
+	    }
+        }
+	else {
+	    continue;
+	}
+    }
+    return temp;
 }
