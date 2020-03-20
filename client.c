e359e9d600769c03cc1c9e55f5ddf491e9c985aa
obliges <rlghks1710@kaist.ac.kr>
2020-03-20 06:34:49 -0700

    1 is finished

--- client.c
+++ client.c
@@ -1,59 +1,60 @@
-#include<string.h>
-#include<ctype.h>
-#include<stdio.h>
-
-char* encrypt(char* plainmessage, int n);
-char* decrypt(char* ciphermessage, int n);
-
-
+char* protocol(char* message, int op, int shift, int checksum);
+unsigned short checksum(const char *buf, unsigned size);
 
 int main(int argc, char **argv) {
-    char koz[100] = "abcdefGHIJKLMNOPQrstuvwxyz";
-
-    printf(encrypt(koz, 1));
-    printf("\n");
-    printf(decrypt(koz, -1));
-    printf("\n");
+    
     return 0;
 }
 
 
-char* encrypt(char* plainmessage, int n) {
-    int move = n%26;
-    char* temp = plainmessage;
-    for (int i = 0; temp[i] != '\0'; i++) {
-	if (isalpha(temp[i])) {
-	    temp[i] = tolower(temp[i]) - (26-move);
-	    if (temp[i] > 'z') {
-		temp[i] = temp[i] - 'z' + 'a' - 1;
-	    }
-	    else if (temp[i] < 'a') {
-		temp[i] = temp[i] + 'z' - 'a' + 1;
-	    }	    	
-	}
-	else {
-	    continue;
-	}
-    }
-    return temp;
+
+char* protocol(char* message, int op, int n, int checksum) {
+
 }
 
-char* decrypt(char* ciphermessage, int n) {
-    int move = n%26;
-    char* temp = ciphermessage;
-    for (int i = 0; temp[i] != '\0'; i++) {
-	if (isalpha(temp[i])) {
-	    temp[i] = tolower(temp[i]) - move;
-	    if (temp[i] > 'z') {
-		temp[i] = temp[i] - 'z' + 'a' - 1;
-	    }
-	    else if (temp[i] < 'a') {
-		temp[i] = temp[i] + 'z' - 'a' + 1;
-	    }
-        }
-	else {
-	    continue;
-	}
+unsigned short checksum(const char *buf, unsigned size) {
+    unsigned long long sum = 0;
+    const unsigned long long *b = (unsigned long long *) buf;
+
+    unsigned t1, t2;
+    unsigned short t3, t4;
+
+    while (size >= sizeof(unsigned long long)) {
+	unsigned long long s = * b++;
+	sum += s;
+	if (sum < s) sum++;
+	size -= 8;
     }
-    return temp;
+
+    buf = (const char *) b;
+    if (size & 4) {
+	unsigned s = *(unsigned *)buf;
+	sum += s;
+	if (sum < s) sum++;
+	buf += 4;
+    }
+
+    if (size & 2) {
+	unsigned short s = *(unsigned short *) buf;
+	sum += s;
+	if (sum < s) sum++;
+	buf += 2;
+    }
+
+    if (size) {
+	unsigned char s = *(unsigned char *) buf;
+	sum += s;
+	if (sum < s) sum++;
+    }
+
+    t1 = sum;
+    t2 = sum >> 32;
+    t1 += t2;
+    if (t1 < t2) t1++;
+    t3 = t1;
+    t4 = t1 >> 16;
+    t3 += t4;
+    if (t3 < t4) t3++;
+
+    return -t3;
 }
--- server.c
+++ server.c
@@ -1,4 +1,68 @@
-int main() {
+#include<string.h>
+#include<ctype.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-	return 0;
+char* encrypt(char* plainmessage, int n);
+char* decrypt(char* ciphermessage, int n);
+
+
+
+
+int main(int argc, char **argv) {
+    char koz[100] = "abcdefghijklmnopqrstuvwxyz013%$#FDKF";
+    char* a = encrypt(koz, 1);
+    char* b = decrypt(koz, 1);
+    printf(a);
+    free(a);
+    printf("\n");
+    printf(b);
+    free(b);
+    printf("\n");
+    return 0;
+}
+
+char* encrypt(char* plainmessage, int n) {
+    int move = n%26;
+    char* temp = malloc(sizeof(char) * strlen(plainmessage));
+    strcpy(temp, plainmessage);
+    //char* temp = plainmessage;
+    for (int i = 0; temp[i] != '\0'; i++) {
+	if (isalpha(temp[i])) {
+	    temp[i] = tolower(temp[i]) - (26-move);
+	    if (temp[i] > 'z') {
+		temp[i] = temp[i] - 'z' + 'a' - 1;
+	    }
+	    else if (temp[i] < 'a') {
+		temp[i] = temp[i] + 'z' - 'a' + 1;
+	    }
+	}
+	else {
+	    continue;
+	}
+    }
+    return temp;
+}
+
+char* decrypt(char* ciphermessage, int n) {
+    int move = n%26;
+    char* temp = malloc(sizeof(char) * strlen(ciphermessage));
+    strcpy(temp, ciphermessage);
+
+    //char* temp = ciphermessage;
+    for (int i = 0; temp[i] != '\0'; i++) {
+	if (isalpha(temp[i])) {
+	    temp[i] = tolower(temp[i]) - move;
+	    if (temp[i] > 'z') {
+		temp[i] = temp[i] - 'z' + 'a' - 1;
+	    }
+	    else if (temp[i] < 'a') {
+		temp[i] = temp[i] + 'z' - 'a' + 1;
+	    }
+	}
+	else {
+	    continue;
+	}
+    }
+    return temp;	
 }
