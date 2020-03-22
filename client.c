e824b1f7da289100f59bf73441d9742934a1f619
obliges <rlghks1710@kaist.ac.kr>
2020-03-22 01:48:00 -0700

    oblige

--- client.c
+++ client.c
@@ -1,17 +1,24 @@
 #include<stdio.h>
-
-
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
 char* protocol(char* message, int op, int shift, int checksum);
 unsigned short checksum(const char *buf, unsigned size);
+char* extract_argument(char** argv, int argc, char* arg);
 
 int main(int argc, char **argv) {
+    struct sockaddr_in serv_addr;
     if (argc != 9) {
 	return 0;
     }
-    char* ip = argv[2];
-    int port = atoi(argv[4]);
-    int op = atoi(argv[6]);
-    int n = atoi(argv[8]);
+    //char* ip = argv[2];
+    char* ip = extract_argument(argv, argc, "-h");    
+    //int port = atoi(argv[4]);
+    int port = atoi(extract_argument(argv, argc, "-p"));
+    //int op = atoi(argv[6]);
+    int op = atoi(extract_argument(argv, argc, "-o"));
+    //int n = atoi(argv[8]);
+    int n = atoi(extract_argument(argv, argc, "-s"));
     printf(ip);
     printf(" ");
     printf("%d", port);
@@ -20,7 +27,13 @@ int main(int argc, char **argv) {
     printf(" ");
     printf("%d", n);
     printf("\n");
-
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    inet_pton(AF_INET, ip, &serv_addr.sin_addr);
+    int clientfd = socket(AF_INET, SOCK_STREAM,0);
+    connect(clientfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    //ssize_t send(int socket, const void *buffer, size_t length, int flags);
     return 0;
 }
 
@@ -75,4 +88,12 @@ unsigned short checksum(const char *buf, unsigned size) {
     if (t3 < t4) t3++;
 
     return -t3;
+}
+
+char* extract_argument(char** argv, int argc, char* arg) {
+    for (int i = 1; i < argc; i = i+2) {
+	if (strcmp(argv[i], arg) == 0) {
+	    return argv[i+1];
+	}
+    }
 }
